cbf3abb71de14eae4471928cb5eebe1a74f25405
Ricardo Gomes <mail@ricardogomes.me>
2016-03-08 07:10:50 -0800

    Added exercise 1 from Worksheet 1 Added README intro for this project

--- FichaPratica1.1/Form1.cs
+++ FichaPratica1.1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace FichaPratica1._1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int bufferSize = 20480;
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = 0;
+            // The file is included in the solution and set to copy to the build folder
+            // To check that the file was copied properly right-click on the project and 
+            // choose Open in File Explorer, then navigate to bin/Debug
+            String originalPath = "security.jpg";
+            String destinationPath = "bak_security.jpg";
+
+            if (File.Exists(destinationPath))
+            {
+                File.Delete(destinationPath);
+            }
+            FileStream originalStream = new FileStream(originalPath, FileMode.Open);
+            FileStream destinationStream = new FileStream(destinationPath, FileMode.Create);
+
+            while ((bytesRead = originalStream.Read(buffer,0,bufferSize)) > 0){
+                destinationStream.Write(buffer, 0, bytesRead);
+            }
+
+            label1.Text = "File Copied ["+originalStream.Length+" bytes]";
+            label1.Visible = true;
+
+            originalStream.Close();
+            destinationStream.Close();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 2015-16
-Repository for the 2015/16 Practice Labs
+# ESTG - EI - SI - 2015-16
+Repository for the 2015/16 Practice Labs 
+
+In this repo you'll find implemented all the proposed exercises.
+
+Normally projects will be added to this repo one week after the release of the exercise sheet.
+
+## Projects
+
+### Worksheet 1
+The Project Worksheet1 refers to exercise 1, points 1 and 2.a. To run make sure is defined as the starting project and hit Start.
