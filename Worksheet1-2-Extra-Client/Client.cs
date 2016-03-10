277491c8ab8659708ffc4474a8b503e083dfad0f
Ricardo Gomes <mail@ricardogomes.me>
2016-03-10 22:50:28 +0000

    Added extra for worksheet 1 exercise  2

---
+++ Worksheet1-2-Extra-Client/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1_2_Extra_Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            int port = 9999;
+            int bufferSize = 1024;
+            int bytesRead;
+            byte[] buffer = new byte[bufferSize];
+            int integerToSend = 42;
+            byte[] encodedInteger = BitConverter.GetBytes(integerToSend);
+            String msg = "SI@EI-15/16";
+            
+            String response;
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, port);
+            TcpClient client = new TcpClient();
+            client.Connect(endpoint);
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Client Started");
+
+            Console.WriteLine("Encrypting String {0}",msg);
+            int key = 5;
+            string encryptedString = CeaserCypherEncrypt(msg, key);
+            byte[] encodedMsg = Encoding.UTF8.GetBytes(encryptedString);
+            Console.WriteLine("Sending String {0}",encryptedString);
+
+            stream.Write(encodedMsg, 0, encodedMsg.Length);
+            bytesRead = stream.Read(buffer, 0, bufferSize);
+            response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received " + response);
+
+          
+
+            stream.Close();
+            client.Close();
+            Console.WriteLine("Client Stoped");
+            Console.ReadKey();
+        }
+
+        static string CeaserCypherEncrypt(string input, int key)
+        {
+            char[] buffer = input.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                letter = (char)(letter + key);
+                if (letter > 65535)
+                {
+                    letter = (char)(letter - 65535);
+                }
+                else if (letter < 0)
+                {
+                    letter = (char)(letter + 65535);
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
+        }
+    }
+}
---
+++ Worksheet1-2-Extra-Server/Server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1_2_Extra_Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = 9999;
+            int bufferSize = 1024;
+            int bytesRead;
+            byte[] buffer = new byte[bufferSize];
+            String ACK = "ACK";
+            byte[] encodedACK = Encoding.UTF8.GetBytes(ACK);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
+            TcpListener listener = new TcpListener(endpoint);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Server Started");
+            bytesRead = stream.Read(buffer, 0, bufferSize);
+            String stringReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received [string] - " + stringReceived);
+            int key = 5;
+            Console.WriteLine("Decrypted [string] - " + CeaserCypherDecrypt(stringReceived,key));
+
+
+            stream.Write(encodedACK, 0, encodedACK.Length);
+
+
+            stream.Close();
+            client.Close();
+            listener.Stop();
+            Console.WriteLine("Server Stoped");
+            Console.ReadKey();
+        }
+
+        static string CeaserCypherDecrypt(string input, int key)
+        {
+            char[] buffer = input.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                letter = (char)(letter - key);
+                if (letter > 65535)
+                {
+                    letter = (char)(letter - 65535);
+                }
+                else if (letter < 0)
+                {
+                    letter = (char)(letter + 65535);
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
+        }
+    }
+}
